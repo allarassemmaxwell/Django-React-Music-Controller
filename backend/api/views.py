76b974b2fc4bb8af99823d53c7f59e3e7722aa32
Allarassem Maxime <allarassemmaxime@Allarassems-MacBook-Air.local>
2022-07-12 13:02:15 +0100

    first commit

---
+++ backend/api/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework import generics
+from .models import Room
+from .serializers import RoomSerializer
+
+
+
+
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
