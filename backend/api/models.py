3a9e3c46d5721416ca80f7c3dcfcec9b6d0027b7
Allarassem Maxime <allarassemmaxime@Allarassems-MacBook-Air.local>
2022-07-13 13:32:03 +0100

    Watching video5-7

--- backend/api/models.py
+++ backend/api/models.py
@@ -19,7 +19,7 @@ def generate_unique_code():
 
 # ROOM MODEL
 class Room(models.Model):
-    code            = models.CharField(max_length=8, default="", unique=True)
+    code            = models.CharField(max_length=8, default=generate_unique_code, unique=True)
     host            = models.CharField(max_length=50, unique=True)
     guest_can_pause = models.BooleanField(default=False, null=False)
     votes_to_skip   = models.IntegerField(null=False, default=1)
