77e4aa0605fd64067e3f34cf6e3023ca0e4a1a47
Allarassem Maxime <allarassemmaxime@Allarassems-MacBook-Air.local>
2022-07-15 11:58:50 +0100

    Watching video8-10

--- backend/frontend/src/components/App.js
+++ backend/frontend/src/components/App.js
@@ -11,7 +11,7 @@ export class App extends Component {
 
     render () {
         return (
-            <div>
+            <div className="center">
                 <HomePage />
             </div>
         );
